3665b779dbfc8ca9c7bc70e4d6bd52a4f93f6690
JoongChangYang <didwndckd@gmail.com>
2020-03-07 16:18:27 +0900

    fixed Dir's

---
+++ OGADA/OGADA/View/BoardingPass/BoardingPassView.swift
@@ -0,0 +1,21 @@
+//
+//  BoardingPass.swift
+//  OGADA
+//
+//  Created by 양중창 on 2020/03/07.
+//  Copyright © 2020 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class BoardingPassView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
