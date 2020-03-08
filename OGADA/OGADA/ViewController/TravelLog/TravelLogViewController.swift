a1003fbdfdc8e2835e952047098ce0fc60604c97
WeatherApp_iOS <eujin8@gmail.com>
2020-03-08 16:45:38 +0900

    TravelLogViewController Update

--- OGADA/OGADA/ViewController/TravelLog/TravelLogViewController.swift
+++ OGADA/OGADA/ViewController/TravelLog/TravelLogViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
+/*
+private func setUI
+    - delegate, dataSource
+    - addsubView
+private func setConstraint
+    - margin, padding 최상단에.
+    - 상수(숫자)를 바로 사용하지 않고 var, let 에 담아서 사용하기!
+
+함수당 간략한 설명 주석 한줄정도.
+
+UI -> setupUI, setupConstraint 두개 합쳐서 마크
+action에 관한 함수 모아놓고  마크
+extension 당 마크 하나씩!!
+*/
+
 class TravelLogViewController: BaseViewController {
 
-    private let budgetView = UIView()
+//    private let budgetView = BudgetView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .background
+
+        setUI()
+        setConstraint()
+        
     }
     
-
+    private func setUI() {
+    }
+    private func setConstraint() {
+        
+    }
    
 
 }
