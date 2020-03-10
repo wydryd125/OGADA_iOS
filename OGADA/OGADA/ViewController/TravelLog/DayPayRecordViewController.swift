a53c944f5dfe8b0e7bfa08df682e85a5ea57eb66
WeatherApp_iOS <eujin8@gmail.com>
2020-03-11 00:48:07 +0900

    DayPayRecordTableViewCell Create

---
+++ OGADA/OGADA/ViewController/TravelLog/DayPayRecordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DayPayRecordViewController.swift
+//  OGADA
+//
+//  Created by YoujinMac on 2020/03/10.
+//  Copyright © 2020 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class DayPayRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
