08132c839d42ac83d4b2200e769616f17ad39328
WeatherApp_iOS <eujin8@gmail.com>
2020-03-11 16:27:18 +0900

    DayPayRecordFooter View create

---
+++ OGADA/OGADA/View/TravelLog/DayPayRecord/DayPayRecordHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  DayPayRecordHeaderView.swift
+//  OGADA
+//
+//  Created by YoujinMac on 2020/03/11.
+//  Copyright © 2020 didwndckd. All rights reserved.
+//
+
+import UIKit
+
+class DayPayRecordHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
