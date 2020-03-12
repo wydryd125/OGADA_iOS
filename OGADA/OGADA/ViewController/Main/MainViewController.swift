a889f86291d5ecc675e3916561aa120bc7e76e24
wydryd125 <ljj4904@gmail.com>
2020-03-12 22:50:21 +0900

    working inputView

--- OGADA/OGADA/ViewController/Main/MainViewController.swift
+++ OGADA/OGADA/ViewController/Main/MainViewController.swift
@@ -11,20 +11,20 @@ import UIKit
 class MainViewController: UIViewController {
     
     private var travels: [TravelInfo] = []
-   
+    
     private enum UI {
-           static let itemsInLine: CGFloat = 2
-           static let linesOnScreen: CGFloat = 2
-           static let itemSpacing: CGFloat = 10.0
-           static let lineSpacing: CGFloat = 10.0
-           static let edgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
-       }
+        static let itemsInLine: CGFloat = 2
+        static let linesOnScreen: CGFloat = 2
+        static let itemSpacing: CGFloat = 10.0
+        static let lineSpacing: CGFloat = 10.0
+        static let edgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
     
     private var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         return UICollectionView(frame: .zero, collectionViewLayout: layout)
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
@@ -53,7 +53,7 @@ class MainViewController: UIViewController {
     private func setUI() {
         
         navigationController?.navigationBar.isHidden = true
-
+        
         collectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: MainCollectionViewCell.identifier)
         collectionView.register(MainButtonCollectionViewCell.self, forCellWithReuseIdentifier: MainButtonCollectionViewCell.identifier)
         collectionView.backgroundColor = .background
@@ -76,6 +76,7 @@ class MainViewController: UIViewController {
     private func pushTravelController() {
         let tabBarController = UITabBarController()
         
+        
         let boardingPassVC = BoardingPassViewController()
         boardingPassVC.tabBarItem.title = "보딩패스"
         
@@ -90,10 +91,10 @@ class MainViewController: UIViewController {
         
         
         tabBarController.viewControllers = [
-        boardingPassVC,
-        movingLineVC,
-        travelLogVC,
-        otherVC
+            boardingPassVC,
+            movingLineVC,
+            travelLogVC,
+            otherVC
         ]
         
         
@@ -105,7 +106,7 @@ class MainViewController: UIViewController {
         
         
     }
-
+    
 }
 
 //MARK: extension
@@ -114,7 +115,7 @@ extension MainViewController: UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return travels.count + 1
     }
-
+    
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         switch indexPath.row {
@@ -122,7 +123,7 @@ extension MainViewController: UICollectionViewDataSource {
             let buttonCell = collectionView.dequeueReusableCell(
                 withReuseIdentifier: MainButtonCollectionViewCell.identifier,
                 for: indexPath
-            ) as! MainButtonCollectionViewCell
+                ) as! MainButtonCollectionViewCell
             buttonCell.delegate = self
             return buttonCell
             
@@ -135,7 +136,7 @@ extension MainViewController: UICollectionViewDataSource {
             let travel = travels[indexPath.row - 1]
             
             cell.configure(travel: travel.nation, departureDate: travel.departureDate, arrivalDate: travel.arrivalDate , sutitle: travel.subTitle)
-
+            
             return cell
         }
     }
@@ -147,9 +148,9 @@ extension MainViewController: UICollectionViewDelegate {
         case 0:
             print("add")
         default:
-//            let movingLingVC = MovingLineViewController()
-//            navigationController?.pushViewController(movingLingVC, animated: false)
-//            keys
+            //            let movingLingVC = MovingLineViewController()
+            //            navigationController?.pushViewController(movingLingVC, animated: false)
+            //            keys
             pushTravelController()
             print("default")
         }
@@ -181,7 +182,7 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
         guard let layout = collectionViewLayout as? UICollectionViewFlowLayout else { fatalError() }
         
         let isVertical = layout.scrollDirection == .vertical
-
+        
         let horizontalSpacing = (isVertical ? itemSpacing : lineSpacing) + horizontalInset
         let verticalSpacing = (isVertical ? lineSpacing : itemSpacing) + verticalInset
         
@@ -200,13 +201,15 @@ extension MainViewController: UICollectionViewDelegateFlowLayout {
     
 }
 extension MainViewController: MainButtonCollectionViewCellDelegate {
- 
+    
     func didTapAddButton() {
-
-        let addTravelVC = AddTravelViewController()
-        navigationController?.pushViewController(addTravelVC, animated: true)
-       
+        let inputVC = InputInfoViewController()
+        navigationController?.pushViewController(inputVC, animated: true)
+        
+//        let addTravelVC = AddTravelViewController()
+//        navigationController?.pushViewController(addTravelVC, animated: true)
+//        
     }
-
+    
 }
 
