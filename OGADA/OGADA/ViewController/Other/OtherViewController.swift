68da9c4b0f700b7aeab110e6d2846e6399737b13
WeatherApp_iOS <eujin8@gmail.com>
2020-03-13 15:44:28 +0900

    modify baseViewController  inherit cancel

--- OGADA/OGADA/ViewController/Other/OtherViewController.swift
+++ OGADA/OGADA/ViewController/Other/OtherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class OtherViewController: BaseViewController {
+class OtherViewController: UIViewController /*BaseViewController*/ {
 
     override func viewDidLoad() {
         super.viewDidLoad()
